7478478e2c8b5dc5d8d64a7219a32828000cfa61
truongnd51 <truongnd51@gmail.com>
2024-02-21 13:48:13 +0700

    truong

--- app/src/main/assets/database.sql
+++ app/src/main/assets/database.sql
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-CREATE TABLE User (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT,
-    first_name TEXT,
-    last_name TEXT,
-    password TEXT,
-    image TEXT,
-    phone_number TEXT,
-    address TEXT
-);
-CREATE TABLE Menu (
-    menu_id INTEGER PRIMARY KEY,
-    name TEXT
-);
-CREATE TABLE Food (
-    food_id INTEGER PRIMARY KEY ,
-    name TEXT,
-    price TEXT,
-    image TEXT,
-    menu_id INTEGER,
-    FOREIGN KEY (menu_id) REFERENCES Menu (menu_id)
-);
-CREATE TABLE Cart (
-    user_id INTEGER,
-    food_id INTEGER,
-    quantity INTEGER,
-    FOREIGN KEY (user_id) REFERENCES User (user_id),
-    FOREIGN KEY (food_id) REFERENCES Food (food_id)
-);
-CREATE TABLE Favourite (
-    user_id INTEGER,
-    food_id INTEGER,
-    FOREIGN KEY (user_id) REFERENCES User (user_id),
-    FOREIGN KEY (food_id) REFERENCES Food (food_id)
-);
-=======
 CREATE TABLE Clinic (
     clinic_id INTEGER PRIMARY KEY AUTOINCREMENT,
     clinic_name TEXT,
@@ -80,4 +43,4 @@ CREATE TABLE Report (
     FOREIGN KEY (disease_id) REFERENCES Disease (disease_id)
 );
 
->>>>>>> origin/master
+
