b2fb05492072dca54df19cef91189eb1f0152d09
truongnd51 <truongnd51@gmail.com>
2024-03-05 18:18:54 +0700

    truong

--- app/src/main/java/com/example/fptproject/databases/repositories/PatientRepository.java
+++ app/src/main/java/com/example/fptproject/databases/repositories/PatientRepository.java
@@ -1,24 +1,15 @@
 package com.example.fptproject.databases.repositories;
 
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
-<<<<<<< HEAD
-import com.example.fptproject.databases.DBHelper;
-import com.example.fptproject.models.Patient;
-
-=======
 
 import com.example.fptproject.databases.DBHelper;
 import com.example.fptproject.models.Patient;
 
 
->>>>>>> origin/master
 public class PatientRepository {
     private final String TABLE_NAME = "Patient";
     private final String COLUMN_USERNAME = "patient_username";
@@ -35,18 +26,12 @@ public class PatientRepository {
         String selection = COLUMN_USERNAME + " = ? AND " + COLUMN_PASSWORD + " = ?";
         String[] selectionArgs = {username, password};
 
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
         Cursor cursor = db.query(TABLE_NAME, null, selection, selectionArgs, null, null, null);
         int count = cursor.getCount();
         cursor.close();
 
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
         // Trả về true nếu có ít nhất một kết quả trả về từ câu truy vấn
         return count > 0;
     }
@@ -58,54 +43,29 @@ public class PatientRepository {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
 
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
         values.put(COLUMN_USERNAME, patient.getUsername());
         values.put(COLUMN_PASSWORD, patient.getPassword());
         values.put(COLUMN_NAME, patient.getName());
         values.put(COLUMN_EMAIL, patient.getEmail());
         values.put(COLUMN_PHONE, patient.getPhone());
 
-<<<<<<< HEAD
-        long newRowId = db.insert(TABLE_NAME, null, values);
-        db.close();
-
-=======
 
         long newRowId = db.insert(TABLE_NAME, null, values);
         db.close();
 
 
->>>>>>> origin/master
         if (newRowId == -1) {
             System.out.println("Failed to add patient to the database.");
         }
     }
 
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
     public boolean isUsernameExists(String username) {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         String selection = COLUMN_USERNAME + " = ?";
         String[] selectionArgs = { username };
 
-<<<<<<< HEAD
-        Cursor cursor = db.query(TABLE_NAME, null, selection, selectionArgs, null, null, null);
-        boolean exists = cursor.getCount() > 0;
-
-        cursor.close();
-        db.close();
-
-        return exists;
-    }
-
-}
-
-=======
 
         Cursor cursor = db.query(TABLE_NAME, null, selection, selectionArgs, null, null, null);
         boolean exists = cursor.getCount() > 0;
@@ -120,4 +80,3 @@ public class PatientRepository {
 
 
 }
->>>>>>> origin/master
