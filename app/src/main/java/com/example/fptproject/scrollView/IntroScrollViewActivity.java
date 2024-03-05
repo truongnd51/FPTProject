9c94d623179334c0ef0386b87778d25ea4c5975e
htcyx <hoangtrancong24092003@gmail.com>
2024-03-05 13:25:41 +0700

    h

---
+++ app/src/main/java/com/example/fptproject/scrollView/IntroScrollViewActivity.java
@@ -0,0 +1,16 @@
+package com.example.fptproject.scrollView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fptproject.R;
+
+public class IntroScrollViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_scroll_view);
+    }
+}
